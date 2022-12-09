b195798ebb415c5102de72f0c416d3ee134e28c8
designsh94 <design__sh@naver.com>
2022-12-09 14:32:17 +0900

    초기 커밋.

---
+++ Client/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+int main()
+{
+	cout << "Function Pointer Networkd(CLIENT) Start" << endl;
+
+
+
+
+
+
+
+
+
+	cout << "Function Pointer Networkd(CLIENT) End" << endl;
+	return 0;
+}
---
+++ Server/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+int main()
+{
+	cout << "Function Pointer Networkd(SERVER) Start" << endl;
+
+
+
+
+
+
+
+
+
+	cout << "Function Pointer Networkd(SERVER) End" << endl;
+	return 0;
+}
